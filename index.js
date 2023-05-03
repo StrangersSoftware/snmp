eab4a7ed7410f5571518e8410dcf5e6be1e80218
Maksym <strangerssoftware@gmail.com>
2023-05-03 10:26:33 +0300

    Add lan adapters and running applications watcher

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
+const network = require('./utils/network')
+const services = require('./utils/services')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -24,7 +26,8 @@ var receiverCallback = function (error, notification) {
   if ( error ) {
       console.error (error);
   } else {
-      console.log (JSON.stringify(notification, null, 2));
+      const output = notification.pdu.varbinds.map((item) => JSON.parse(item.value))
+      console.log('RECEIVER MESSAGE -', output);
   }
 };
 
@@ -67,30 +70,6 @@ app.post('/', (req, res) => {
     });
     session.close();
   });
-
-
-  var varbinds = [
-    {
-        oid: "1.3.6.1.4.1.2000.2",
-        type: snmp.ObjectType.OctetString,
-        value: "Hardware health status changed"
-    },
-    {
-        oid: "1.3.6.1.4.1.2000.3",
-        type: snmp.ObjectType.OctetString,
-        value: "status-error"
-    }
-  ];
-
-  var trapOid = "1.3.6.1.4.1.2000.1";
-
-  // version 2c should have been specified when creating the session
-  session.trap (trapOid, varbinds, function (error) {
-    if (error)
-        console.error (error);
-    console.log('TRAP');
-  });
-
 });
 
 app.get('/services', async (req, res) => res.render('pages/services', {
@@ -103,44 +82,25 @@ app.get('/services', async (req, res) => res.render('pages/services', {
 app.post('/services', (req, res) => {
   const ip = req.body.ip;
   const name = req.body.name;
+  const installed = req.body.installed === 'clicked';
 
   const session = snmp.createSession(ip || "127.0.0.1", "public");
-  var oid = "1.3.6.1.2.1.25.4.2";
-  var columns = [2, 6, 7];
+  var oid = installed ? "1.3.6.1.2.1.25.6.3" : "1.3.6.1.2.1.25.4.2";
+            
+  var columns = installed ? [2] : [2, 4, 6, 7];
 
   const responseCb = (error, table) => {
-    if (error) {
-        console.error (error.toString ());
-    } else {
-        var indexes = [];
-        for (let index in table) indexes.push (parseInt (index));
-        
-        const oids_data = indexes.map((index)=> ({ 
-          oid: `1.3.6.1.2.1.25.4.2.1.3.${index}`, 
-          resp: table[index][columns[0]], 
-          runType: table[index][columns[1]],
-          status: table[index][columns[2]] 
-        }));
-
-        const search_oid = indexes
-          .filter((index)=> table[index][columns[0]].includes(name))
-          .map((index) => ({ 
-              oid: `1.3.6.1.2.1.25.4.2.1.3.${index}`, 
-              resp: table[index][columns[0]], 
-              runType: table[index][columns[1]],
-              status: table[index][columns[2]] 
-            }));
-
-        res.render('pages/services', {
-          oids: !!name ? search_oid : oids_data,
-          tagline: `The following software found on requested host (${ip || "127.0.0.1"})`,
-          subTagline: 'Run type: running(1), runnable(2), notRunnable(3), invalid(4); Status:  running(1), runnable(2), notRunnable(3), invalid(4)',
-        });
-    }
+    const rez = installed ? services.getInstalled(error, table) : services.getRunning(error, table);
+    const search_oid = name && rez.filter((index)=> index.resp.toString().includes(name));
+    res.render('pages/services', {
+      oids: name ? search_oid : rez,
+      tagline: `The following software found on requested host (${ip || "127.0.0.1"})`,
+      subTagline: 'Run type: unknown(1), operatingSystem(2), deviceDriver(3), application(4); \n Status:  running(1), runnable(2), notRunnable(3), invalid(4)',
+    });
   }
 
   var maxRepetitions = 1;
-  console.log('SERVICES TABLE');
+  console.log(installed ? 'INSTALLED APPLICATIONS' : 'RUNNING SERVICES TABLE');
   session.tableColumns(oid, columns, maxRepetitions, responseCb);
 });
 
@@ -197,26 +157,15 @@ app.post('/network', (req, res) => {
 
   const session = snmp.createSession(ip || "127.0.0.1", "public");
   var oid = "1.3.6.1.2.1.2.2";
-  var columns = [2, 7, 8];
-  const responseCb = (error, table) => {
-    if (error) {
-        console.error (error.toString ());
-    } else {
-        var indexes = [];
-        for (let index in table) indexes.push (parseInt (index));
+  var columns = [2, 7, 8, 6];
 
-        const oids_data = indexes.map((index)=> ({ 
-          oid: `1.3.6.1.2.1.2.2.1.2.${index}`, 
-          resp: table[index][columns[0]], 
-          status: table[index][columns[1]],
-          opStatus: table[index][columns[2]],
-        }));
-        res.render('pages/network', {
-          oids: oids_data,
-          tagline: `The following network adapters found on requested host (${ip || "127.0.0.1"})`,
-          subTagline: `Statuses:  up(1), down(2), testing(3), unknown(4), dormant(5), notPresent(6), lowerLayerDown(7)`,
-        });
-    }
+  const responseCb = (error, table) => {
+    const rez = network.getAdapters(error, table);
+    res.render('pages/network', {
+      oids: rez,
+      tagline: `The following network adapters found on requested host (${ip || "127.0.0.1"})`,
+      subTagline: `Statuses:  up(1), down(2), testing(3), unknown(4), dormant(5), notPresent(6), lowerLayerDown(7)`,
+    });
   }
 
   var maxRepetitions = 1;
@@ -239,9 +188,94 @@ app.post('/network', (req, res) => {
       }
     });
   }
+  
   console.log('NETWORK TABLE');
   session.tableColumns(oid, columns, maxRepetitions, responseCb);
 });
+
+
+var global_net_adapters = [];
+var global_running_apps = [];
+
+setInterval(() => {
+  const session = snmp.createSession("127.0.0.1", "public");
+  var network_oid = "1.3.6.1.2.1.2.2";
+  var columns = [2, 7, 8, 6];
+  let rez = [];
+
+  const checkAdapters = (error, table) => {
+    const current = network.getAdapters(error, table);
+    
+    for(let i = 0; i < current.length; i++ ) 
+      if (JSON.stringify(current[i]) !== JSON.stringify(global_net_adapters[i]))
+        rez.push({...current[i], resp: current[i].resp.toString().slice(0, -1)});
+    
+    if  (rez.length && global_net_adapters.length) {
+      console.log('AGENT REPORT: network adapters table change');
+
+      var options = {agentAddr: '127.0.0.1', upTime: 1000};
+      var enterpriseOid = "1.3.6.1.4.1.2000.1";
+
+      var varbinds = rez.map((item) => ({
+        oid: item.oid,
+        type: snmp.ObjectType.OctetString,
+        value: JSON.stringify(item),
+      }));
+
+      session.trap (enterpriseOid, varbinds, options,
+              function (error) {
+          if (error)
+              console.error (error);
+      });
+    }
+
+    global_net_adapters = [...current];
+  }
+  session.tableColumns(network_oid, columns, 1, checkAdapters);
+
+  var running_apps_oid = "1.3.6.1.2.1.25.4.2";
+  var apps_columns = [2, 4, 6, 7];
+
+  const name = 'Telegram';
+
+  rez = [];
+
+  const checkApps = (error, table) => {
+    const apps = services.getRunning(error, table).map(service => service.resp.toString());
+
+    const newApps = apps.filter(element => !global_running_apps.includes(element));
+    const closedApps = global_running_apps.filter(element => !apps.includes(element));
+
+    if  ((newApps.length || closedApps.length) && global_running_apps.length) {
+      console.log('AGENT REPORT: running applications change');
+
+      const options = {agentAddr: '127.0.0.1', upTime: 1000};
+      
+      const appOid = "1.3.6.1.4.1.2001.1";
+
+      const message = {
+        started: newApps,
+        closed: closedApps,
+      }
+
+      var appVarbind = {
+        oid: appOid,
+        type: snmp.ObjectType.OctetString,
+        value: JSON.stringify(message),
+      };
+
+      session.trap(appOid, [appVarbind], options,
+              function (error) {
+          if (error)
+              console.error (error);
+      })
+    }
+
+    global_running_apps = [...apps];
+  }
+
+  session.tableColumns(running_apps_oid, apps_columns, 1, checkApps);
+}, 3000);
 
 app.get('/about', function(req, res) {
   res.render('pages/about');
